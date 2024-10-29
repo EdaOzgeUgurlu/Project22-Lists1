517e4de9b55cf8af073cd0e11c8ffd053c16eedf
Eda Özge UĞURLU <edaozgeugurlu@gmail.com>
2024-10-29 23:08:14 +0300

    Proje dosyası ekle.

---
+++ Project22-Lists1/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] davetliler = new string[8]; // dizi oluşturuyoruz.
+Console.WriteLine("davetlilerin isimlerini yazınız");
+for (int i = 0; i < davetliler.Length; i++)
+{
+    
+    string davetli = Console.ReadLine();
+    davetliler[i] = davetli; //kullanıcıdan alınan her davetliyi davetliler listesine atıyoruz.
+
+}
+
+foreach (string item in davetliler) //davetli isimlerini yazdıyoruz
+{ 
+    Console.WriteLine(item);
+}
